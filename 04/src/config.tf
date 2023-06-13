7bcc25260d93fce15f190d0ca2a74e1f2e9a0fbe
george25031996 <75673715+george25031996@users.noreply.github.com>
2023-06-13 15:18:20 +0300

    Update config.tf

--- 04/src/config.tf
+++ 04/src/config.tf
@@ -3,8 +3,8 @@ data template_file "userdata" {
 
   vars = {
     username = var.username
-    ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJcdO44CubnrhkN6V2sqoVnUY87iTBow9uz73gvEmxl egor.solt@yandex.ru"
-    ssh_key = "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZWQyNTUxOQAAACBiXHTuOArm564ZDeldrKqFZ1GPO4kwaMPbs+94LxJsZQAAAJhICfsKSAn7CgAAAAtzc2gtZWQyNTUxOQAAACBiXHTuOArm564ZDeldrKqFZ1GPO4kwaMPbs+94LxJsZQAAAEBwRF1tuWP/ATc2hqRINYj+Fo1UCHYa/m7vr9U97yZJqGJcdO44CubnrhkN6V2sqoVnUY87iTBow9uz73gvEmxlAAAAE2Vnb3Iuc29sdEB5YW5kZXgucnUBAg=="
+    ssh_public_key = ""
+    ssh_key = ""
     }
 }
 
